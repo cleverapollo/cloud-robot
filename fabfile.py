e60d73b8456d00986603bb17a9253959ecae99d9
cleverapollo <cleverapollo@gmail.com>
2018-04-14 19:35:46 +0100

    Linting

--- fabfile.py
+++ fabfile.py
@@ -9,25 +9,25 @@ env.forward_agent = True
 
 @task
 def staging():
-	"""
-	Setup the staging environment (Robot Alpha)
-	:usage: `fab staging <cmd>`
-	"""
-	env.environment = 'staging'
-	env.hosts = ['2a02:2078:3::3']
-	env.branch = 'master'
-	env.home = '/home/administrator'
-	setup()
+    """
+    Setup the staging environment (Robot Alpha)
+    :usage: `fab staging <cmd>`
+    """
+    env.environment = 'staging'
+    env.hosts = ['2a02:2078:3::3']
+    env.branch = 'master'
+    env.home = '/home/administrator'
+    setup()
 
 def setup():
-	"""Setup the required paths for fabric to deploy to"""
-	env.root = os.path.join(env.home, env.project)
+    """Setup the required paths for fabric to deploy to"""
+    env.root = os.path.join(env.home, env.project)
 
 @task
 def deploy():
-	require('root', provided_by=('staging', 'live'))
-	require('branch', provided_by=('staging', 'live'))
-	with cd(env.root):
-		run('git fetch origin')
-		run('git reset --hard origin/%(branch)s' % env)
-		
+    require('root', provided_by=('staging', 'live'))
+    require('branch', provided_by=('staging', 'live'))
+    with cd(env.root):
+        run('git fetch origin')
+        run('git reset --hard origin/%(branch)s' % env)
+        
