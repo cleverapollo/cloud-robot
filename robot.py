c3713eec6a6d97f58c9264adc9b695526080bee5
cleverapollo <cleverapollo@gmail.com>
2018-04-21 14:59:21 +0100

    Increase wait time back to 20 seconds

--- robot.py
+++ robot.py
@@ -53,7 +53,7 @@ def mainloop(watcher: INotify):
         else:
             robot_logger.info('No VMs in "Requested" state.')
 
-        while last > time.time() - 2:
+        while last > time.time() - 20:
             time.sleep(1)
         last = time.time()
 
