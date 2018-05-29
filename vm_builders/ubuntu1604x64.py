44221181153dd51a217a1056e64b18febb134e49
cleverapollo <cleverapollo@gmail.com>
2018-05-29 11:49:25 +0100

    Minor touchups in the Ubuntu16 builder

--- vm_builders/ubuntu1604x64.py
+++ vm_builders/ubuntu1604x64.py
@@ -6,67 +6,69 @@ from crypt import crypt, mksalt, METHOD_SHA512
 import utils
 
 # kickstarts files path
-path = "/mnt/images/kickstarts/"
+path = '/mnt/images/kickstarts/'
 driver_logger = utils.get_logger_for_name('ubuntu1604x64.vm_build')
 
 
 def answer_file(vm: dict) -> str:
     """
-    creates a answerfile data from given vm
-    :param vm: dict
-    :return: ks_text: string
+    Creates an answer file to be used to create the VM specified
+    :param vm: Data for the VM that will be created
+    :return: ks_text: The answer file that can build the specified VM
     """
-    ks_text = ''
-    comment = f"# {vm['idImage']} Kickstart for VM {vm['vmname']} \n"
-    ks_text += comment
+    ks_text = f'# {vm["idImage"]} Kickstart for VM {vm["vmname"]} \n'
     # System authorization information
-    ks_text += "auth --useshadow --enablemd5\n"
+    ks_text += 'auth --useshadow --enablemd5\n'
     # System language
-    ks_text += "lang " + str(vm['lang']) + "\n"
+    ks_text += f'lang {vm["lang"]}\n'
     # Language modules to install
-    ks_text += "langsupport " + str(vm['lang']) + "\n"
+    ks_text += f'langsupport {vm["lang"]}\n'
     # System keyboard
-    ks_text += "keyboard " + str(vm['keyboard']) + "\n"
+    ks_text += f'keyboard {vm["keyboard"]}\n'
     # System mouse
-    ks_text += "mouse\n"
+    ks_text += 'mouse\n'
     # System timezone
-    ks_text += "timezone " + str(vm['tz']) + "\n"
+    ks_text += f'timezone {cm["tz"]}\n'
     # Root password
-    ks_text += "rootpw --iscrypted {0}\n".format(vm['root_pw'])
+    ks_text += f'rootpw --iscrypted {vm["root_pw"]}'
     # username and password
-    ks_text += "user administrator --fullname \"" + str(vm['u_name']) \
-               + "\" --password " + str(vm['u_passwd']) + "\n"
+    ks_text += (
+        f'user administrator --fullname "{vm["u_name"]}" --password '
+        f'{vm["u_password"]}\n'
+    )
     # Reboot after installation
-    ks_text += "reboot\n"
+    ks_text += 'reboot\n'
     # Use text mode install
-    ks_text += "text\n"
+    ks_text += 'text\n'
     # Install OS instead of upgrade
-    ks_text += "install\n"
+    ks_text += 'install\n'
     # Installation media
-    ks_text += "cdrom\n"
+    ks_text += 'cdrom\n'
     # System bootloader configuration
-    ks_text += "bootloader --location=mbr\n"
+    ks_text += 'bootloader --location=mbr\n'
     # Clear the Master Boot Record
-    ks_text += "zerombr yes\n"
-    ks_text += "autopart\n"
+    ks_text += 'zerombr yes\n'
+    ks_text += 'autopart\n'
     # Partition clearing information
-    ks_text += "clearpart --all --initlabel\n"
+    ks_text += 'clearpart --all --initlabel\n'
     # Basic disk partition
-    ks_text += "part / --fstype ext4 --size 1 --grow --asprimary\n"
-    ks_text += "part swap --size 1024\n"
-    ks_text += "part / boot --fstype ext4 --size 256 --asprimary\n"
+    ks_text += 'part / --fstype ext4 --size 1 --grow --asprimary\n'
+    ks_text += 'part swap --size 1024\n'
+    ks_text += 'part / boot --fstype ext4 --size 256 --asprimary\n'
     # System authorization infomation
-    ks_text += "auth --useshadow --enablemd5\n"
+    ks_text += 'auth --useshadow --enablemd5\n'
     # Network
-    ks_text += "network --bootproto=static --ip=" + str(vm['ip']) + \
-               " --netmask=" + str(vm['netmask_ip']) + " --gateway=" + \
-               str(vm['gateway']) + " --nameserver=" + str(vm['dns']) + "\n"
+    ks_text += (
+        f'network --bootproto=static --ip={vm["ip"]} --netmask='
+        f'{vm["netmask_ip"]} --gateway={vm["gateway"]} --nameserver='
+        f'{vm["dns"]}\n'
+    )
     # Do not configure the X Window System
-    ks_text += "iskipx\n"
+    ks_text += 'iskipx\n'
     # post installation
-    ks_text += "%packages\n"
-    ks_text += "@ ubuntu-server\n"
-    ks_text += "@ openssh-server\n"
+    ks_text += '%%packages\n'
+    ks_text += '@ ubuntu-server\n'
+    ks_text += '@ openssh-server\n'
 
     return ks_text
 
@@ -83,8 +85,8 @@ def vm_build(vm: dict, password: str) -> bool:
     vm['root_pw'] = str(crypt(vm['r_passwd'], mksalt(METHOD_SHA512)))
     vm['user_pw'] = str(crypt(vm['u_passwd'], mksalt(METHOD_SHA512)))
     ks_text = answer_file(vm)
-    ks_file = str(vm['name']) + ".cfg"
-    with open(path + ks_file, 'w') as ks:
+    ks_file = f'{vm['name']}.cfg'
+    with open(f'{path}{ks_file}', 'w') as ks:
         ks.write(ks_text)
     try:
         client = paramiko.SSHClient()
@@ -93,15 +95,15 @@ def vm_build(vm: dict, password: str) -> bool:
                        password=password)
 
         # make the cmd
-        cmd = "sudo virt-install --name " + vm['name'] + " --memory " + \
-              str(vm['ram']) + " --vcpus " + str(vm['cpu']) + \
-              " --disk path=/var/lib/libvirt/images/" + str(vm['name']) + \
-              ".qcow2,size=" + str(vm['hdd']) + \
-              " --graphics vnc --location /mnt/images/" + \
-              str(vm['image']).replace(' ', '\ ') + ".iso" + \
-              " --os-variant rhel6 --initrd-inject " + path + ks_file + \
-              " -x  \"ks=file:/" + ks_file + "\" --network bridge=br" + \
-              str(vm['vlan'])
+        image_replaced = vm['image'].replace(' ', r'\ ')
+        cmd = (
+            f'sudo virt-install --name {vm["name"]} --memory {vm["ram"]} '
+            f'--vcpus {vm["cpu"]} --disk path=/var/lib/libvirt/images/'
+            f'{vm["name"]}.qcow2,size={vm["hdd"]} --graphics vnc --location '
+            f'/mnt/images/{image_replaced}.iso --os-variant '
+            f'rhel6 --initrd-inject {path}{ks_file} -x "ks=file:/{ks_file}" '
+            f'--network bridge=br{vm["vlan"]}'
+        )
         stdin, stdout, stderr = client.exec_command(cmd)
         if stdout:
             for line in stdout:
@@ -110,7 +112,9 @@ def vm_build(vm: dict, password: str) -> bool:
         elif stderr:
             driver_logger.error(stderr)
     except Exception:
-        driver_logger.exception("Exception occurred during SSHing into host")
+        driver_logger.exception(
+            f'Exception occurred during SSHing into host {vm["host_ip"]}'
+        )
 
     finally:
         client.close()
