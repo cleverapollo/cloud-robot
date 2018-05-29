a50514070e4d7f4fba92bd70c0fa4aeffa23bae6
cleverapollo <cleverapollo@gmail.com>
2018-05-29 14:45:53 +0100

    Minor touchups to ro.py

--- ro.py
+++ ro.py
@@ -7,10 +7,7 @@
 # python
 import random
 import string
-import winrm
 
-from base64 import b64encode
-from datetime import datetime
 from typing import Optional
 
 # libs
@@ -22,146 +19,159 @@ import utils
 TOKEN_WRAPPER = utils.Token()
 
 
-def service_entity_create(service: str,
-                          entity: str,
-                          params: dict) -> Optional[list]:
+def service_entity_create(
+        service: str, entity: str, data: dict) -> Optional[dict]:
     """
-    Create given entity into a service with params
-    :param service: string
-    :param entity: string
-    :param params: dict
-    :return: list or empty string
+    Generalised method for creation of an entity
+    :param service: The api service the entity belongs to
+    :param entity: The entity to create an instance of
+    :param data: The data to be used to create the entity instance
+    :return: A dict containing the newly created instance's data, or None if
+             nothing was created
     """
+    logger = utils.get_logger_for_name('ro.service_entity_create')
+    logger.info(
+        f'Attempting to create an instance of {service}.{entity} with the '
+        f'following data: {data}'
+    )
     entity_create = None
     service_to_call = getattr(api, service)
     # service_to_call = api.iaas  (e.g service = 'iaas')
-    entity_to_call = getattr(service_to_call(), entity)
+    entity_to_call = getattr(service_to_call, entity)
     # entity_to_call = api.iaas.image (e.g entity = 'image')
-    response = entity_to_call.create(token=TOKEN_WRAPPER.token, params=params)
+    response = entity_to_call.create(token=TOKEN_WRAPPER.token, data=data)
     if response.status_code == 201:
         entity_create = response.json()['content']
-    elif response.status_code == 400:
-        utils.get_logger_for_name('ro.service_entity_create').error(
-            f"An error occurred while creating {entity.upper()} "
-            f"object in {service.upper()} service with params: {str(params)},"
-            f" response code={response.status_code} and "
-            f"Details: {response.json()['detail']}")
+        logger.info(
+            f'Successfully created an instance of {service}.{entity} with the '
+            f'following data: {data}'
+        )
     else:
-        utils.get_logger_for_name('ro.service_entity_create').error(
-            f"An error occurred while creating {entity.upper()} "
-            f"object in {service.upper()} service with params: {str(params)},"
-            f" response code={response.status_code}"
+        logger.error(
+            f'HTTP Error {response.status_code} occurred while trying to '
+            f'create an instance of {service}.{entity} with the following data'
+            f': {data}.\nResponse from API: {response.content.decode()}'
         )
     return entity_create
 
 
 def service_entity_list(service: str, entity: str, params: dict) -> list:
     """
-    Gets the list of given entity and params from given service
-    :param service: string
-    :param entity: string
-    :param params: dict
-    :return: list
+    Retrieves a list of instances of a given entity in a service, which can
+    be filtered
+    :param service: The api service the entity belongs to
+    :param entity: The entity type to list instances of
+    :param params: Search parameters to be passed to the list call
+    :return: A list of instances of service.entity
     """
-    entity_list = list()
+    logger = utils.get_logger_for_name('ro.service_entity_list')
+    logger.info(
+        f'Attempting to retrieve a list of {service}.{entity} records with the'
+        f' following params : {params}'
+    )
     service_to_call = getattr(api, service)
     # service_to_call = api.iaas  (e.g service = 'iaas')
-    entity_to_call = getattr(service_to_call(), entity)
+    entity_to_call = getattr(service_to_call, entity)
     # entity_to_call = api.iaas.image (e.g entity = 'image')
     response = entity_to_call.list(token=TOKEN_WRAPPER.token, params=params)
-    if response.status_code == 200:
-        entity_list.extend(response.json()['content'])
-    elif response.status_code == 400:
-        utils.get_logger_for_name('ro.service_entity_list').error(
-            f"An error occurred while fetching {entity.upper()} "
-            f"list from {service.upper()} service with params {str(params)},"
-            f" response code={response.status_code} and "
-            f"Details: {response.json()['detail']}"
+    if response.status_code != 200:
+        logger.error(
+            f'HTTP Error {response.status_code} occurred while trying to '
+            f'list instances of {service}.{entity} with the following params'
+            f': {params}.\nResponse from API: {response.content.decode()}'
         )
-    else:
-        utils.get_logger_for_name('ro.service_entity_list').error(
-            f"An error occurred while fetching {entity.upper()} "
-            f"list from {service.upper()} service with params {str(params)},"
-            f" response code={response.status_code}"
-        )
-    if response.json()['_metadata']['totalRecords'] > 0:
-        utils.get_logger_for_name('ro.service_entity_list').info(
-            f"{len(entity_list)} {entity.upper()}s were found!"
-        )
-    else:
-        utils.get_logger_for_name('ro.service_entity_list').info(
-            f"No requested {entity.upper()}s were found!"
-        )
-    return entity_list
+        return []
+    records_found = response.json()['_metadata']['totalRecords']
+    plural = records_found != 1
+    logger.info(
+        f'Found {records_found} instance{"s" if plural else ""} of '
+        f'{service}.{entity} with the following params : {params}'
+    )
+    return response.json()['content']
 
 
-def service_entity_update(service: str, entity: str, params: dict) -> [1, 0]:
+def service_entity_update(
+        service: str, entity: str, pk: int, data: dict) -> bool:
     """
-    Updates given entity and params from given service
-    :param service: string
-    :param entity: string
-    :param params: dict
-    :return: 1 or 0
+    Update an instance of service.entity with the specified pk using the
+    supplied data
+    :param service: The api service the entity belongs to
+    :param entity: The entity type to update
+    :param pk: The id of the entity to update
+    :param data: The data to use for updating the instance
+    :return: Flag stating whether or not the update was successful
     """
+    logger = utils.get_logger_for_name('ro.service_entity_update')
+    logger.info(
+        f'Attempting to update the {service}.{entity} instance #{pk} '
+        f'with the following data : {data}'
+    )
     service_to_call = getattr(api, service)
     # service_to_call = api.iaas  (e.g service = 'iaas')
-    entity_to_call = getattr(service_to_call(), entity)
+    entity_to_call = getattr(service_to_call, entity)
     # entity_to_call = api.iaas.image (e.g entity = 'image')
-    params['data']['updated'] = datetime.utcnow()
     response = entity_to_call.partial_update(
-        pk=params['pk'], token=TOKEN_WRAPPER.token, data=params['data'])
+        pk=pk,
+        token=TOKEN_WRAPPER.token,
+        data=data
+    )
     # Checking just updation no return of data so 204 No content
     if response.status_code == 204:
-        utils.get_logger_for_name('ro.service_entity_update').info(
-            f"Updated successfully {entity} with id:{params['pk']} "
-            f"in {service}"
+        logger.info(
+            f'Successfully updated {service}.{entity} instance #{pk} '
+            f'with the following data : {data}'
         )
-        return 1
+        return True
     else:
-        utils.get_logger_for_name('ro.service_entity_update').error(
-            f"An error occurred while updating {entity} in "
-            f"{service} service \n {response.content}"
-            f"Error code: {response.status_code}"
+        logger.error(
+            f'HTTP Error {response.status_code} returned when attempting to '
+            f'update {service}.{entity} instance #{pk}\nResponse from API: '
+            f'{response.content.decode()}'
         )
-        return 0
+        return False
 
 
-def service_entity_read(service: str, entity: str, params: dict) -> dict:
+def service_entity_read(
+        service: str, entity: str, pk: int) -> Optional[dict]:
     """
-    Reads given entity and params from given service
-    :param service: string
-    :param entity: string
-    :param params: dict
-    :return: dict
+    Read an instance of service.entity with the specified pk and return it,
+    using params to filter if necessary
+    :param service: The api service the entity belongs to
+    :param entity: The entity type to read
+    :param pk: The id of the entity to read
+    :return: A dict containing the data of the read instance, or None if an
+             error occurs
     """
-    entity_read = dict()
+    logger = utils.get_logger_for_name('ro.service_entity_read')
+    logger.info(f'Attempting to read the {service}.{entity} instance #{pk}')
     service_to_call = getattr(api, service)
     # service_to_call = api.iaas  (e.g service = 'iaas')
-    entity_to_call = getattr(service_to_call(), entity)
+    entity_to_call = getattr(service_to_call, entity)
     # entity_to_call = api.iaas.image (e.g entity = 'image')
-    response = entity_to_call.read(pk=params['pk'], token=TOKEN_WRAPPER.token)
+    response = entity_to_call.read(pk=pk, token=TOKEN_WRAPPER.token)
     if response.status_code == 200:
-        entity_read = response.json()['content']
-    elif response.status_code == 400:
-        utils.get_logger_for_name('ro.service_entity_read').error(
-            f"An error occurred while reading {entity} in {service} service"
-            f"Details: {response.json()['detail']}"
-        )
+        logger.info(f'Successfully read {service}.{entity} instance #{pk}')
+        return response.json()['content']
     else:
-        utils.get_logger_for_name('ro.service_entity_read').error(
-            f"An error occurred while reading {entity} in {service} service"
-            f"Error code: {response.status_code}"
+        logger.error(
+            f'HTTP Error {response.status_code} returned when attempting to '
+            f'read {service}.{entity} instance #{pk}\nResponse from API: '
+            f'{response.content.decode()}'
         )
-    return entity_read
+        return None
 
 
-def get_idrac_details(location: str) -> (str, str):
+def get_idrac_details(location: str) -> Optional[(str, str)]:
     """
-    Gets the idrac ip address and password of the asset
-    given its location as argument
-    :param location: string eg "CIX1AGU12"
-    :return: ip: ipaddress(string) and password: string
+    Generate the ip address and password of the iDRAC interface of an asset,
+    given its location
+    :param location: string stating the location of the asset in CIX style
+                     (eg "CIX1AGU12")
+    :return: A tuple containing the ip_address and password for the interface,
+             or None if an error occurs with the API
     """
+    logger = utils.get_logger_for_name('ro.get_idrac_details')
+    logger.info(f'Attempting to generate iDRAC info for location {location}')
     location = location.replace(" ", "")  # Remove white spaces
     # Find the ipaddress
     if location[:5] == "CIX1A":
@@ -171,22 +181,31 @@ def get_idrac_details(location: str) -> (str, str):
     elif location[:5] == "CIX1C":
         ip = "10.251"
     else:
-        ip = "Undefined"
+        # Undefined location, return error
+        logger.error(
+            f'Location {location} did not contain one of the valid room '
+            f'identifiers (CIX1A, CIX1B, CIX1C)'
+        )
+        return
 
-    base = 0
-    if ip != "Undefined":
-        if location[5] in ["A", "G", "M"]:
-            base = 0
-        if location[5] in ["B", "H", "N"]:
-            base = 20
-        if location[5] in ["C", "I", "O"]:
-            base = 40
-        if location[5] in ["D", "J", "P"]:
-            base = 60
-        if location[5] in ["E", "K", "Q"]:
-            base = 80
-        if location[5] in ["F", "L", "R"]:
-            base = 80
+    if location[5] in ["A", "G", "M"]:
+        base = 0
+    elif location[5] in ["B", "H", "N"]:
+        base = 20
+    elif location[5] in ["C", "I", "O"]:
+        base = 40
+    elif location[5] in ["D", "J", "P"]:
+        base = 60
+    elif location[5] in ["E", "K", "Q"]:
+        base = 80
+    elif location[5] in ["F", "L", "R"]:
+        base = 80
+    else:
+        logger.error(
+            f'Location {location} did not contain one of the valid rack '
+            f'identifiers (A-R)'
+        )
+        return
     rack = location.split(location[:5])[-1]
     ip += '.' + str(base + int(rack[1: 3]))
     ip += '.' + rack[rack.index('U') + 1:]
@@ -194,80 +213,71 @@ def get_idrac_details(location: str) -> (str, str):
     # Get the password
     response = api.iaas.location_hasher.create(token=TOKEN_WRAPPER.token,
                                                data={'location': location})
-    password = ''
     if response.status_code in [200, 201]:
         password = response.json()['content']['hexadecimal']
-    else:
-        utils.get_logger_for_name('ro.get_idrac_details').error(
-            f"Error generating host password for location: {location}"
+        logger.info(
+            f'Successfully generated iDRAC details for location {location}'
         )
+    else:
+        logger.error(
+            f'Error generating host password for location {location}\n'
+            f'Response from location_hasher API: {response.content.decode()}'
+        )
+        return
     return ip, password
 
 
-def ip_validations(address_range: str, ip_address: str) -> Optional[dict]:
+def ip_validator(
+        address_range: str = '', ip_address: str = '') -> Optional[dict]:
     """
-    This method is used to validate either address_range given in string,
-    and separated by commas, if two or more addresses ranges to be validated
-    in one request.
-    also validates ipaddress given in string,
-    and separated by commas, if two or more ipaddresses to be validated in
-    one request.
-    :param address_range: string
-    :param ip_address:string
-    :return:dict
+    This method acts as a wrapper around the api.iaas.ip_validator endpoint.
+    This endpoint validates one or more address ranges and ip addresses,
+    which are passed in the form of a string where multiple addresses are
+    comma separated
+    :param address_range: optional string containing one or more addressRanges
+    :param ip_address: optional string containing one or more ipAddresses
+    :return: The response from the ip_validator API or None if an error occurs
     """
+    logger = utils.get_logger_for_name('ro.ip_validations')
+    logger.info(
+        'Attempting to validate the following information: addressRanges:'
+        f' {address_range}, ipAddresses: {ip_address}'
+    )
     params = dict()
     if address_range:
-        params['address_range'] = address_range
+        params['addressRanges'] = address_range
     if ip_address:
-        params['ip_address'] = ip_address
-    try:
-        response = api.iaas.ip_validator.list(token=TOKEN_WRAPPER.token,
-                                              params=params)
-        try:
-            if response.json()['response_code'] == 400:
-                utils.get_logger_for_name('ro.ip_validations').error(
-                    f"400 Error, No result found, please check the "
-                    f"Error: {response.json()['detail']}"
-                )
-                return None
-        except Exception:
-            utils.get_logger_for_name('ro.ip_validations').exception(
-                "Successfully validated")
-            return response.json()
-    except Exception:
-        utils.get_logger_for_name('ro.ip_validations').exception(
-            f"Error occurred while requesting ip_validator of iaas"
-        )
-    utils.get_logger_for_name('ro.ip_validations').error(
-        f"Nothing found while requesting ip_validator of iaas"
+        params['ipAddresses'] = ip_address
+    # Send a request to the API to validate the ipAddresses and addressRanges
+    response = api.iaas.ip_validator.list(
+        token=TOKEN_WRAPPER.token,
+        params=params
     )
-    return None
+    if response.status_code == 200:
+        logger.info(
+            'Obtained a successful response from the API for the following '
+            f'information: addressRanges: {address_range}, ipAddresses: '
+            f'{ip_address}'
+        )
+        return response.json()['content']
+    else:
+        logger.error(
+            f'HTTP ERROR {response.status_code} received when attempting to'
+            f' validate the following information: addressRanges: '
+            f'{address_range}, ipAddresses: {ip_address}'
+        )
 
 
-def fix_run_ps(session: winrm.Session.run_ps,
-               script: str) -> winrm.Response:
-    """
-    winrm supporting function, dont make anychanges
-    :param script:
-    :return:
-    """
-    encoded_ps = b64encode(script.encode('utf_16_le')).decode('ascii')
-    rs = session.run_cmd(f'powershell -encodedcommand {encoded_ps}')
-    if len(rs.std_err):
-        rs.std_err = session.clean_error_msg(rs.std_err.decode('utf-8'))
-    return rs
-
-
-def password_generator(size: int = 8,
-                       chars: Optional[str] = None) -> str:
+def password_generator(
+        size: int = 8, chars: Optional[str] = None) -> str:
     """
     Returns a string of random characters, useful in generating temporary
     passwords for automated password resets.
 
-    size: default=8; override to provide smaller/larger passwords
-    chars: default=A-Za-z0-9; override to provide more/less diversity
+    :param size: default=8; override to provide smaller/larger passwords
+    :param chars: default=A-Za-z0-9; override to provide more/less diversity
+    :return: A password of length 'size' generated randomly from 'chars'
     """
     if chars is None:
         chars = string.ascii_letters + string.digits
-    return ''.join(random.choice(chars) for i in range(size))
+    return ''.join(random.choice(chars) for _ in range(size))
