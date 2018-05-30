8f9ca8ed641c69ddf5056e752652f1deaf06208a
cleverapollo <cleverapollo@gmail.com>
2018-05-30 12:56:38 +0100

    RobotAlpha has to use live creds

--- settings.py
+++ settings.py
@@ -2,18 +2,18 @@
 # Alpha Robot Credentials
 
 # LIVE SETTINGS
-# CLOUDCIX_API_URL = 'https://api.cloudcix.com/'
-# CLOUDCIX_API_USERNAME = 'robot@alpha.cloudcix.com'
-# CLOUDCIX_API_PASSWORD = 'C1x@lphA'
-# CLOUDCIX_API_KEY = '3bc7cc2bddb34d78b31f1223d0a7408e'
-# CLOUDCIX_AUTH_URL = 'https://keystone.cloudcix.com:5000/v3/'
-# NETWORK_PASSWORD = 'C1xacc355'
-
-
-# # STAGE SETTINGS
-CLOUDCIX_API_URL = 'https://stageapi.cloudcix.com/'
-CLOUDCIX_AUTH_URL = 'https://stagekeystone.cloudcix.com:35357/v3/'
+CLOUDCIX_API_URL = 'https://api.cloudcix.com/'
 CLOUDCIX_API_USERNAME = 'robot@alpha.cloudcix.com'
 CLOUDCIX_API_PASSWORD = 'C1x@lphA'
 CLOUDCIX_API_KEY = '3bc7cc2bddb34d78b31f1223d0a7408e'
+CLOUDCIX_AUTH_URL = 'https://keystone.cloudcix.com:5000/v3/'
 NETWORK_PASSWORD = 'C1xacc355'
+
+
+# # STAGE SETTINGS
+# CLOUDCIX_API_URL = 'https://stageapi.cloudcix.com/'
+# CLOUDCIX_AUTH_URL = 'https://stagekeystone.cloudcix.com:35357/v3/'
+# CLOUDCIX_API_USERNAME = 'robot@alpha.cloudcix.com'
+# CLOUDCIX_API_PASSWORD = 'C1x@lphA'
+# CLOUDCIX_API_KEY = '3bc7cc2bddb34d78b31f1223d0a7408e'
+# NETWORK_PASSWORD = 'C1xacc355'
