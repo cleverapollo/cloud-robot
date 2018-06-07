03c064a5d7c6c54b071d4087c5b97a71aa8cd893
cleverapollo <cleverapollo@gmail.com>
2018-06-07 13:03:32 +0100

    Linting metrics/heartbeat.py

--- metrics/heartbeat.py
+++ metrics/heartbeat.py
@@ -11,13 +11,13 @@ def heartbeat(value: int = 1):
     """
     client = utils.get_influx_client()
     data = [{
-        "measurement": "robot_heartbeat",
-        "tags": {
-            "region": settings.REGION_NAME
+        'measurement': 'robot_heartbeat',
+        'tags': {
+            'region': settings.REGION_NAME
         },
-        "time": datetime.utcnow(),
-        "fields": {
-            "value": value
+        'time': datetime.utcnow(),
+        'fields': {
+            'value': value
         }
     }]
     client.write_points(data)
