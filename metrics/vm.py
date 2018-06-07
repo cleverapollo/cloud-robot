32abe3ca0326423117163612ab0dd35cea61f415
cleverapollo <cleverapollo@gmail.com>
2018-06-07 14:52:25 +0100

    Linting metrics/vm.py

--- metrics/vm.py
+++ metrics/vm.py
@@ -7,7 +7,7 @@ def build_success():
     Sends a data packet to Influx reporting a successful build
     """
     client = utils.get_influx_client()
-    data = generate_data_packet("vm_success", value=1)
+    data = generate_data_packet('vm_success', value=1)
     client.write_points(data)
 
 
@@ -16,5 +16,5 @@ def build_failure():
     Sends a data packet to Influx reporting a successful build
     """
     client = utils.get_influx_client()
-    data = generate_data_packet("vm_failure", value=1)
+    data = generate_data_packet('vm_failure', value=1)
     client.write_points(data)
