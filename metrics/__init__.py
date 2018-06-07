c03bb4814e14a478ce8a6c3ef8c51601bc1501ff
cleverapollo <cleverapollo@gmail.com>
2018-06-07 14:51:00 +0100

    Imported the vrf and vm methods into the __init__ file so they can be accessed elsewhere

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,6 +1,18 @@
 from .heartbeat import heartbeat
+from .vrf import (
+    build_failure as vrf_failure,
+    build_success as vrf_success,
+)
+from .vm import (
+    build_failure as vm_failure,
+    build_success as vm_success,
+)
 
 __all__ = [
     # metric methods
     'heartbeat',
+    'vrf_failure',
+    'vrf_success',
+    'vm_failure',
+    'vm_success',
 ]
