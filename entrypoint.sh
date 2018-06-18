6d6403729b553842b94f76d4e3ae40f537054ccd
cleverapollo <cleverapollo@gmail.com>
2018-06-18 17:47:21 +0100

    edit entrypoint script to catch and pass on SIGTERMS to the python script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
+$CHILD_PID=""
 # Link the settings file to the correct place
 cp "deployment/settings/$ROBOT_ENV.py" ./settings.py
+# Create a method to catch SIGTERMs and pass them to the python process
+handle_term() {
+    kill -TERM "$CHILD_PID"
+}
+trap handle_term SIGTERM
 # Run the robot script
-python3 robot.py
+python3 -u robot.py &
+CHILD_PID=$!
+wait "$CHILD_PID"
