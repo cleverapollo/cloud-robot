0880c7aebf088fe4271e639f6016d9f438748987
cleverapollo <cleverapollo@gmail.com>
2018-06-18 16:58:17 +0100

    Removed inotify watching from mainloop now that it's being handled by Docker

--- robot.py
+++ robot.py
@@ -5,9 +5,6 @@ import subprocess
 import sys
 import time
 
-# lib
-from inotify_simple import INotify, flags
-
 # local
 import dispatcher
 import metrics
@@ -19,41 +16,13 @@ import utils
 robot_logger = utils.get_logger_for_name('robot.mainloop')
 
 
-def watch_directory() -> INotify:
-    """
-    Watches the robot directory for changes.
-    If a change is deteced, spawn a new Robot instance and kill this one
-    :returns: An Inotify instance that can be used to tell if the directory
-              has changed
-    """
-    inotify = INotify()
-    # Create flags for the usual things a deployment will change
-    watch_flags = flags.CREATE | flags.DELETE | flags.MODIFY
-    inotify.add_watch('.', watch_flags)
-    return inotify
-
-
-def mainloop(watcher: INotify, process_pool: mp.Pool):
+def mainloop(process_pool: mp.Pool):
     """
     The main loop of the Robot program
     """
     last = time.time()
     while True:
         metrics.heartbeat()
-        # First check to see if there have been any events
-        if watcher.read(timeout=1000):
-            robot_logger.info('Update detected. Spawning New Robot.')
-            # Wait until all the deployment is finished
-            time.sleep(10)
-            # Spawn a new robot process in the background
-            subprocess.Popen(
-                ['python3', 'robot.py'],
-                env=os.environ,
-            )
-            # Wait a couple of seconds for the new robot to take over
-            time.sleep(2)
-            # Exit this process gracefully
-            sys.exit(0)
         # Now handle the loop events
         # #################  VRF BUILD ######################################
         vrfs = ro.service_entity_list('IAAS', 'vrf', params={'state': 1})
@@ -114,7 +83,7 @@ if __name__ == '__main__':
     )
     rc = 0
     try:
-        mainloop(watch_directory(), pool)
+        mainloop(pool)
     except KeyboardInterrupt:
         # Going down safely
         pass
