20124f0781cffe87bab57016197da1a0d2954eaa
cleverapollo <cleverapollo@gmail.com>
2018-07-11 17:34:03 +0100

    Updated metrics

--- metrics/vm.py
+++ metrics/vm.py
@@ -1,20 +1,15 @@
-import utils
-from ._helpers import generate_data_packet
+from cloudcix_metrics import prepare_metrics, Metric
 
 
 def build_success():
     """
     Sends a data packet to Influx reporting a successful build
     """
-    client = utils.get_influx_client()
-    data = generate_data_packet('vm_success', value=1)
-    client.write_points(data)
+    prepare_metrics(lambda: Metric('vm_success', 1, {}))
 
 
 def build_failure():
     """
-    Sends a data packet to Influx reporting a successful build
+    Sends a data packet to Influx reporting a failed build
     """
-    client = utils.get_influx_client()
-    data = generate_data_packet('vm_failure', value=1)
-    client.write_points(data)
+    prepare_metrics(lambda: Metric('vm_failure', 1, {}))
