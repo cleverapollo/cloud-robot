8864d589b5bfc8c52cc53321da63a057fdecf81f
cleverapollo <cleverapollo@gmail.com>
2018-07-30 14:56:48 +0100

    Added region name tag

--- metrics/vrf.py
+++ metrics/vrf.py
@@ -1,15 +1,16 @@
 from cloudcix_metrics import prepare_metrics, Metric
+from ..settings import REGION_NAME
 
 
 def build_success():
     """
     Sends a data packet to Influx reporting a successful build
     """
-    prepare_metrics(lambda: Metric('vrf_success', 1, {}))
+    prepare_metrics(lambda: Metric('vrf_success', 1, {'region': REGION_NAME}))
 
 
 def build_failure():
     """
     Sends a data packet to Influx reporting a failed build
     """
-    prepare_metrics(lambda: Metric('vrf_failure', 1, {}))
+    prepare_metrics(lambda: Metric('vrf_failure', 1, {'region': REGION_NAME}))
