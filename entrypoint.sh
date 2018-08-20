1a4d503a962b845ea8b43480b214a75c94f0d480
cleverapollo <cleverapollo@gmail.com>
2018-08-20 14:20:49 +0100

    Small (hopefully the last) fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 $CHILD_PID=""
 # Link the settings file to the correct place
 cp "deployment/settings/$ROBOT_ENV.py" ./settings.py
-install -o $(id -u) -g $(id -g) -m 600 "deployment/ssh-keys/$ROBOT_ENV" ~/.ssh/id_rsa
+install -m 600 "deployment/ssh-keys/$ROBOT_ENV" ~/.ssh/id_rsa
+# Ensure the running user owns everything in the ssh folder
+chown $(id -u):$(id -g) -R ~/.ssh
 # Delete the deployment folder after copying
 rm -rf deployment
 # Create a method to catch SIGTERMs and pass them to the python process
