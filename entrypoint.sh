d99bcca80fd13a39ed3036010ae3c2dc5ce6c3c3
cleverapollo <cleverapollo@gmail.com>
2018-09-03 11:26:27 +0100

    Refactored Dockerfiles into a separate file for each region

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,4 @@
 CHILD_PID=""
-# Link the settings file to the correct place
-cp "deployment/settings/$ROBOT_ENV.py" ./settings.py
-install -m 600 "deployment/ssh-keys/$ROBOT_ENV" ~/.ssh/id_rsa
-# Ensure the running user owns everything in the ssh folder
-chown $(id -u):$(id -g) -R ~/.ssh
-# Delete the deployment folder after copying
-rm -rf deployment
 # Create a method to catch SIGTERMs and pass them to the python process
 handle_term() {
     kill -TERM "$CHILD_PID"
