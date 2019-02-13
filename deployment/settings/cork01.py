13222c54f89a0ffdb3729707143858c620c9b10d
cleverapollo <cleverapollo@gmail.com>
2019-02-13 13:19:39 +0000

    Cork01

---
+++ deployment/settings/cork01.py
@@ -0,0 +1,17 @@
+# Alpha Robot Settings
+
+# CloudCIX API Settings
+# Login username
+CLOUDCIX_API_USERNAME = 'robot@cork01.cloudcix.com'
+# Login password
+CLOUDCIX_API_PASSWORD = 'C1xC0rk01'
+
+# Logging Settings
+# Name of region (used to tag data sent to influx)
+REGION_NAME = 'cork01'
+
+# Webmail settings
+# Region's email id
+CLOUDCIX_EMAIL_USERNAME = 'cork01@cloudcix.com'
+# Region's email password
+CLOUDCIX_EMAIL_PASSWORD = 'C0rk01C1xacc355'
