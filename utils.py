83da6254caeaf090e621160d46db9762b743156b
cleverapollo <cleverapollo@gmail.com>
2019-05-02 18:52:05 +0100

    Last little cleanups

--- utils.py
+++ utils.py
@@ -41,9 +41,6 @@ def setup_root_logger():
     instead of having that old bad system
     :param level: The level at which to log messages
     """
-    # set root logger level to critical to prevent double logging
-    logging.getLogger().setLevel(logging.CRITICAL)
-
     # Set up robot parent logger
     logger = logging.getLogger('robot')
     logger.setLevel(logging.DEBUG)
