29284a1d219dd4efff1e91b39e550934ed50912d
cleverapollo <cleverapollo@gmail.com>
2019-05-09 10:26:29 +0100

    Added inventory and playbook files for worker deployments
    Separated them into different files so we can track workers separately

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,3 +1,32 @@
 """
 Module containing all of the non beat celery tasks
+
+In this file, we define the robot based tasks that will be run by celery beat
 """
+# stdlib
+from datetime import datetime, timedelta
+# local
+import settings
+from celery_app import app, robot
+
+
+@app.task
+def mainloop():
+    """
+    Run one instance of the Robot mainloop
+    """
+    robot()
+
+
+@app.task
+def scrub_loop():
+    """
+    Once per day, at midnight, call the robot scrub methods to delete hardware
+    """
+    robot.logger.info('Commencing scrub task')
+    # Add the Scrub timestamp when the region isn't Alpha
+    timestamp = None
+    if settings.REGION_NAME != 'alpha':
+        timestamp = (datetime.now() - timedelta(days=30)).isoformat()
+    robot.vrf_scrub(timestamp)
+    robot.vm_scrub(timestamp)
