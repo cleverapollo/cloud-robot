6f863e4a17e6833a3e6c34abe472a52964c09717
cleverapollo <cleverapollo@gmail.com>
2020-03-09 18:57:41 +0000

    management ip of router is now taken from Router object

--- scrubbers/virtual_router.py
+++ scrubbers/virtual_router.py
@@ -103,20 +103,10 @@ class VirtualRouter(VirtualRouterMixin):
 
         data['project_id'] = virtual_router_data['project']['id']
 
-        # Get the management ip address which is IPv6 and Gateway as name of Router ips
-        management_ip = None
+        # Get the management ip address from the Router.
         child_span = opentracing.tracer.start_span('reading_router', child_of=span)
         router = utils.api_read(Compute.router, virtual_router_data['router_id'], span=child_span)
         child_span.finish()
-        for ip in router['ip_addresses']:
-            if IPAddress(ip['address']).version == 6 and ip['name'] == 'Gateway':
-                management_ip = ip['address']
-                break
-        if management_ip is None:
-            VirtualRouter.logger.error(
-                f'Mangement ip address not found for the Router # {router["id"]}',
-            )
-            return None
-        data['management_ip'] = management_ip
+        data['management_ip'] = router['management_ip']
 
         return data
