ab26625026422d8edc93b28adbd868ccd780759c
cleverapollo <cleverapollo@gmail.com>
2020-03-23 15:00:55 +0000

    use kickstarts in directory path

--- builders/vm/windows.py
+++ builders/vm/windows.py
@@ -244,7 +244,7 @@ class Windows(WindowsMixin):
         Windows.logger.debug(f'Generated answer file for VM #{vm_id}\n{answer_file_data}')
         try:
             # Attempt to write
-            answer_file_path = f'{network_drive_path}/answer_files/{template_data["vm_identifier"]}.xml'
+            answer_file_path = f'{network_drive_path}/kickstarts/{template_data["vm_identifier"]}.xml'
             with open(answer_file_path, 'w') as f:
                 f.write(answer_file_path)
             Windows.logger.debug(f'Successfully wrote answer file for VM #{vm_id} to {answer_file_data}')
