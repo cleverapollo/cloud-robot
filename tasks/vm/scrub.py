0b5db5be1bdb018123909efc02434626d9e09464
cleverapollo <cleverapollo@gmail.com>
2020-10-23 13:37:38 +0100

    Py3 iaas

--- tasks/vm/scrub.py
+++ tasks/vm/scrub.py
@@ -126,7 +126,7 @@ def _scrub_vm(vm_id: int, span: Span):
         response = IAAS.vm.delete(token=Token.get_instance().token, pk=vm_id, span=child_span)
         child_span.finish()
 
-        if response.status_code != 200:
+        if response.status_code != 204:
             logger.error(
                 f'HTTP {response.status_code} error occurred when attempting to delete VM #{vm_id};\n'
                 f'Response Text: {response.content.decode()}',
